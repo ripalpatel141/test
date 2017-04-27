b1c0ac5cc4d91b4e5ba55aa382ddd288be363610
EXTTCS27 <EXTTCS27@TATAUS-FX1KM32.moog.com>
2017-04-27 16:03:21 -0400

    Cleanup

--- src/main/java/org/domain/Resource.java
+++ src/main/java/org/domain/Resource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.GET;
 
-@Path("/service1")
+@Path("/service")
 public class Resource {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
